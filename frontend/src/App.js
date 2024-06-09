44af8e5a772327eacee971381350109338c8d2c2
BharathSai3693 <saibharath3693@gmail.com>
2024-06-09 20:11:36 +0530

    Basic Giftpage, Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { AuthProvider, useAuth } from "./AuthContext"; // Import AuthProvider
@@ -8,6 +13,8 @@ import Home from "./pages/Home";
 import AdminRoute from "./Routes/AdminRoute";
 import AdminDashboard from "./pages/AdminDashboard";
 import Items from "./pages/Items";
+import Giftpage from "./pages/Giftpage";
+import ShoppingCart from "./pages/ShoppingCart";
 
 const App = () => {
   return (
@@ -17,15 +24,21 @@ const App = () => {
           <Navbar />
           <hr></hr>
           <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/items" element={<Items />} />
-            <Route path="/additems" element={
-              <AdminRoute>
-                <AdminDashboard />
-              </AdminRoute>
-            }/> 
+            <Route path="/gift" element={<Giftpage />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+            
+            <Route
+              path="/additems"
+              element={
+                <AdminRoute>
+                  <AdminDashboard />
+                </AdminRoute>
+              }
+            />
           </Routes>
         </div>
       </AuthProvider>
